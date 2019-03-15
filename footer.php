07adaa07ff2e3f4e7b33eabba378608e551d9e27
Nikesh <nikesh9220@gmail.com>
2019-03-16 01:43:00 +0530

    Theme Added

---
+++ footer.php
@@ -0,0 +1,90 @@
+<?php
+ include "header.php"
+?>
+
+<!-- === BEGIN FOOTER === -->
+<div id="base">
+    <div class="container bottom-border padding-vert-30">
+        <div class="row">
+            <!-- Disclaimer -->
+            <div class="col-md-4">
+                <h3 class="class margin-bottom-10">Disclaimer</h3>
+                <p>All Parking Does not contain smart mode only saveral configured by latest technology</p>
+                </div>
+            <!-- End Disclaimer -->
+            <!-- Contact Details -->
+            <div class="col-md-4 margin-bottom-20">
+                <h3 class="margin-bottom-10">Contact Details</h3>
+                <p>
+                    <span class="fa-phone">Telephone:</span>+91 7041791111
+                    <br>
+                    <span class="fa-envelope">Email:</span>
+                    <a href="mailto:info@example.com">admin@paypark.com</a>
+                    <br>
+                    <span class="fa-link">Website:</span>
+                    <a href="http://www.example.com">www.payandpark.tk</a>
+                </p>
+                <p>The Admin,
+                    <br>Caspia,
+                    <br>Blossom Aura,
+                    <br>Nadiad</p>
+            </div>
+            <!-- End Contact Details -->
+            <!-- Sample Menu -->
+            <div class="col-md-4 margin-bottom-20">
+                <h3 class="margin-bottom-10">Menu</h3>
+                <ul class="menu">
+                    <li>
+                        <a class="fa-tasks" href="#">Daily Available</a>
+                    </li>
+                    <li>
+                        <a class="fa-users" href="#">Non-Listed</a>
+                    </li>
+                    <li>
+                        <a class="fa-signal" href="#">Mostly Used</a>
+                    </li>
+                    <li>
+                        <a class="fa-coffee" href="#">Smart Parking</a>
+                    </li>
+                </ul>
+                <div class="clearfix"></div>
+            </div>
+            <!-- End Sample Menu -->
+        </div>
+    </div>
+</div>
+<!-- Footer -->
+<div id="footer" class="background-grey">
+    <div class="container">
+        <div class="row">
+            <!-- Footer Menu -->
+            <!-- End Footer Menu -->
+            <!-- Copyright -->
+            <div id="copyright" class="col-md-4">
+                <p class="pull-right"> &nbsp; &nbsp; &nbsp; &nbsp &nbsp(c) 2018 Pay & Park</p>
+            </div>
+            <!-- End Copyright -->
+        </div>
+    </div>
+</div>
+<!-- End Footer -->
+<!-- JS -->
+<script type="text/javascript" src="assets/js/jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="assets/js/scripts.js"></script>
+<!-- Isotope - Portfolio Sorting -->
+<script type="text/javascript" src="assets/js/jquery.isotope.js" type="text/javascript"></script>
+<!-- Mobile Menu - Slicknav -->
+<script type="text/javascript" src="assets/js/jquery.slicknav.js" type="text/javascript"></script>
+<!-- Animate on Scroll-->
+<script type="text/javascript" src="assets/js/jquery.visible.js" charset="utf-8"></script>
+<!-- Sticky Div -->
+<script type="text/javascript" src="assets/js/jquery.sticky.js" charset="utf-8"></script>
+<!-- Slimbox2-->
+<script type="text/javascript" src="assets/js/slimbox2.js" charset="utf-8"></script>
+<!-- Modernizr -->
+<script src="assets/js/modernizr.custom.js" type="text/javascript"></script>
+<!-- End JS -->
+</body>
+</html>
+<!-- === END FOOTER === -->
---
+++ header.php
@@ -0,0 +1,101 @@
+<!-- === BEGIN HEADER === -->
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+    <!--<![endif]-->
+    <head>
+        <!-- Title -->
+        <title>Pay and Park</title>
+        <!-- Meta -->
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <!-- Favicon -->
+        <link href="favicon.ico" rel="shortcut icon">
+        <!-- Bootstrap Core CSS -->
+        <link rel="stylesheet" href="assets/css/bootstrap.css" rel="stylesheet">
+        <!-- Template CSS -->
+        <link rel="stylesheet" href="assets/css/animate.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/font-awesome.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/nexus.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/responsive.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/custom.css" rel="stylesheet">
+        <!-- Google Fonts-->
+        <link href="http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div id="body-bg">
+            <!-- Phone/Email -->
+            <div id="pre-header" class="background-gray-lighter">
+                <div class="container no-padding">
+                    <div class="row hidden-xs">
+                        <div class="col-sm-6 padding-vert-5">
+                            <strong>Phone:</strong>&nbsp;7041791111
+</div>
+                        <div class="col-sm-6 text-right padding-vert-5">
+                            <strong>Email:</strong>&nbsp;admin@paypark.com
+</div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Phone/Email -->
+            <!-- Header -->
+            <div id="header">
+                <div class="container">
+                    <div class="row">
+                        <!-- Logo -->
+                        <div class="logo">
+                            <a href="index.html" title="">
+                                <img src="assets/img/typo/logo3.png" alt="Logo" />
+                            </a>
+                        </div>
+                        <!-- End Logo -->
+                    </div>
+                </div>
+            </div>
+            <!-- End Header -->
+            <!-- Top Menu -->
+            <div id="hornav" class="bottom-border-shadow">
+                <div class="container no-padding border-bottom">
+                    <div class="row">
+                        <div class="col-md-8 no-padding">
+                            <div class="visible-lg">
+                                <ul id="hornavmenu" class="nav navbar-nav">
+                                    <li>
+                                        <a href="index.php" class="fa-home active">Home</a>
+                                    </li>
+                                    <li>
+                                        <a href="login.php"  class="fa-angle-double-down ">Login</a>
+                                    </li>
+                                    <li><a href="signup.php"  class="fa-angle-double-right ">Sign Up</a>
+                                                                            </li>
+                                                                       <li>
+                                        <a href="contact.html" class="fa-comment ">Contact</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-4 no-padding">
+                            <ul class="social-icons pull-right">
+                                <li class="social-rss">
+                                    <a href="#" target="_blank" title="RSS"></a>
+                                </li>
+                                <li class="social-twitter">
+                                    <a href="#" target="_blank" title="Twitter"></a>
+                                </li>
+                                <li class="social-facebook">
+                                    <a href="#" target="_blank" title="Facebook"></a>
+                                </li>
+                                <li class="social-googleplus">
+                                    <a href="#" target="_blank" title="Google+"></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Top Menu -->
+            <!-- === END HEADER === -->
